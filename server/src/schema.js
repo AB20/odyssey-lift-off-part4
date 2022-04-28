b7d4562744fc407ec2cdcb10ec532ef69e84a9cd
Anthony Barnes <abarnes204@aol.com>
2022-04-28 19:51:33 -0400

    update Mutation - create type IncrementTrackViewsResponse

--- server/src/schema.js
+++ server/src/schema.js
@@ -53,9 +53,20 @@ const typeDefs = gql`
     videoUrl: String
   }
 
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
+
   type Mutation { 
     "Increment the number of views of a track"
-    incrementTrackViews(id: ID!)
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
   }
 `;
 
